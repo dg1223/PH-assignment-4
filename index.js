4401b91516ee06de124c21bcb0142225dd90d7f5
dg1223 <alavi1223@hotmail.com>
2023-02-07 01:00:24 -0500

    problem 4; array.some

--- index.js
+++ index.js
@@ -50,16 +50,16 @@ function evenOdd(string) {
 // Third problem: isLGSeven
 function isLGSeven(number) {
     /**
-    * The isLGSeven function returns the result of subtracting 7 from a number,
-    * or multiplying the number by 2 if it is greater than seven.
-    * 
-    *
-    * @param number Pass in the number that is being tested
-    *
-    * @return The difference between the number and 7 if it is less than 7;
-    *         otherwise double the number
-    *
-    */
+     * The isLGSeven function returns the result of subtracting 7 from a number,
+     * or multiplying the number by 2 if it is greater than seven.
+     * 
+     *
+     * @param number Pass in the number that is being tested
+     *
+     * @return The difference between the number and 7 if it is less than 7;
+     *         otherwise double the number
+     *
+     */
 
     if ((typeof number) !== 'number') {
         return "TypeError: Your input should only be a number."
@@ -69,15 +69,45 @@ function isLGSeven(number) {
     return ((difference < 7) ? difference : number*2)
 }
 
-a = isLGSeven(0)
-console.log(a)
-
 // Fourth problem: findingBadData
-function findingBadData(params) {
-    return NULL
+function findingBadData(array) {
+    /**
+     * The findingBadData function counts the number of negative values 
+     * in an array.
+     * 
+     *
+     * @param array An array containing only numbers
+     *
+     * @return The number of negative values in the input array
+     *
+     */
+
+    // check if input is an array
+    if (!Array.isArray(array)) {
+        return "TypeError: Your input should only be an array."
+    }
+
+    // check if any array item is not a number
+    const num_type = (item) => typeof item !== 'number'
+    if (array.some(num_type)){
+        return "TypeError: Your array should contain numbers only."
+    }
+
+    // count negative values
+    let bad_data = 0
+    for(let i = 0; i < array.length; i++){
+        if (array[i] < 0) {
+            bad_data += 1
+        }
+    }
+
+    return bad_data
 }
 
 // Fifth problem: gemsToDiamond
 function gemsToDiamond(params) {
     return NULL
 }
+
+a = findingBadData([-4, -9, -5, -33, -55])
+console.log(a)
